d5b17719e23fc20ed129b7d833dc7290ddc87c38
natnael-meresa <natnaelmeresa6@gmail.com>
2022-01-18 20:25:37 +0300

    bug fix add cors

--- server/index.js
+++ server/index.js
@@ -10,15 +10,13 @@ connectDB()
 const app =  express();
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(function(req, res, next) {
+    req.headers['if-none-match'] = 'no-match-for-this';
+    next();    
+  });
+import cors from'cors'
+  app.use(cors());
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
-if(process.env.NODE_ENV === 'production' ){
-    app.use(express.static(path.join(__dirname, '/client/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
 
 import userRoutes from './routes/userRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
@@ -27,6 +25,14 @@ app.use('/api/upload', uploadRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/user', userRoutes)
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/client/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))

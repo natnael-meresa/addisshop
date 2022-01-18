093185ec180bc9a213f0db707ae17c1097a09e38
natnael-meresa <natnaelmeresa6@gmail.com>
2022-01-18 20:27:08 +0300

    ui change for product

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,8 +1,10 @@
-
 import React from 'react';
 import {Card} from 'react-bootstrap';
 import Rating from './Rating'
-import { Link } from 'react-router-dom';
+
+import {
+    Link,
+  } from "react-router-dom";
 const Product = ({product}) => {
     let date = new Date(product.updatedAt);
     let months = [ "January", "February", "March", "April", "May", "June", 
@@ -11,19 +13,19 @@ const Product = ({product}) => {
     return (
         <Card className="ml-3">
             <div className="thumb-content">
-                <a href="single.html">
+                <Link to="#">
                     <Card.Img variant="top" className="product-img" src={product.image} />
-                </a>
+                </Link>
             </div>
             <Card.Body >
-                <Card.Title><a href="single.html">{product.name}</a></Card.Title>
+                <Card.Title><Link to="#">{product.name}</Link></Card.Title>
                 <Card.Text className="card-body-product">
                     <ul className="list-inline product-meta">
                         <li className="list-inline-item">
-                            <a href="single.html"><i class="far fa-folder-open"></i>{product.category}</a>
+                            <Link to="#"><i class="far fa-folder-open"></i>{product.category}</Link>
                         </li>
                         <li className="list-inline-item">
-                            <a href="category.html"><i className="fa fa-calendar"></i> {date.getDay()+1}th {months[date.getMonth()]}</a>
+                            <Link to="#"><i className="fa fa-calendar"></i> {date.getDay()+1}th {months[date.getMonth()]}</Link>
                         </li>
                     </ul>
                     <div className="product-ratings">
